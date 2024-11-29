37466ce23f3f304746be3c85106ab9654c3d33de
Brendan <bgmcmullen@comcast.net>
2024-11-28 19:43:14 -0800

    added brick animation

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,27 @@ function App() {
   const [playerPlayMessage, setPlayerPlayMessage] = useState<string>('');
   const [takeFromPile, setTakeFromPile] = useState<string>('discard');
   const [nextBrick, setNextBrick] = useState<number | null>(null);
+  const [playerTowerStatus, setPlayerTowerStatus] = useState<object>({brickAnimation: undefined, towerAnimation: undefined});
+  const [computerTowerStatus, setComputerTowerStatus] = useState<object>({brickAnimation: undefined, towerAnimation: undefined});
+
+
+  function calculateTowerStatus(tower: number[]) {
+    let errors = 0;
+
+    for (let i = 0; i < tower.length - 1; i++) {
+      console.log(tower[i]);
+      if (tower[i] > tower[i + 1]){
+        errors++;
+      }
+    }
+    console.log('errors', errors)
+    if (errors <= 1)
+      return {brickAnimation: 'flash 2s ease-in-out infinite', towerAnimation: undefined};
+    else if (errors <= 3)
+      return {brickAnimation: undefined, towerAnimation: undefined};
+    else
+      return {brickAnimation: undefined, towerAnimation: 'sway 2s ease-in-out infinite'};
+  }
 
 
   useEffect(() => {
@@ -87,6 +108,7 @@ function App() {
     })
 
     setMessageQueue((prevMessageQueue) => [...prevMessageQueue, getMessage]);
+
   }
 
   function sendStartMessasge() {
@@ -104,7 +126,13 @@ function App() {
 
     setMessageQueue((prevMessageQueue) => [...prevMessageQueue, message]);
 
+
   }
+
+  useEffect(() => {
+    setComputerTowerStatus(calculateTowerStatus(computerTower));
+    setPlayerTowerStatus(calculateTowerStatus(playerTower));
+  }, [computerTower, playerTower])
 
   function switchToMainPile() {
     setTakeFromPile('main');
@@ -138,19 +166,24 @@ function App() {
 
   return (
     <>
+    <div>computerTowerStatus: {computerTowerStatus.towerAnimation}</div>
+    <div>playerTowerStatus: {playerTowerStatus.towerAnimation}</div>
       <br></br>
       {computerPlayMessage}
       <>Computer's Tower:
         <br></br>
-        {computerTower.map((brick) => <div className="button-container"><button style={{ width: 5 + (brick * 4) }}>{brick}</button></div>
-        )} </>
+        <div className='tower' style={{animation: computerTowerStatus.towerAnimation}}>
+        {computerTower.map((brick, index) => <div key={`computer-brick-container${index}`} className="button-container"><button key={`computer-brick${index}`} className='tower-button' style={{ width: 5 + (brick * 4), animation: computerTowerStatus.brickAnimation, animationDelay:`${index/10}s` }}>{brick}</button></div>
+        )} 
+        </div></>
       <br></br>
       <div>{playerPlayMessage}</div>
       <>Player's Tower:
         <br></br>
         <div>Next Brick: {nextBrick}</div>
-        {playerTower.map((brick) => <div className="button-container"><button style={{ width: 5 + (brick * 4) }} onClick={sendReplaceBrickMessage}>{brick}</button></div>
-        )} <button onClick={switchToMainPile}>Take from Main</button></>
+        <div className='tower' style={{animation: playerTowerStatus.towerAnimation}}>
+        {playerTower.map((brick, index) => <div key={`player-brick-container${index}`} className="button-container"><button key={`player-brick${index}`} className='tower-button' style={{ width: 5 + (brick * 4), animation: playerTowerStatus.brickAnimation, animationDelay:`${index/10}s` }} onClick={sendReplaceBrickMessage}>{brick}</button></div>
+        )} <button onClick={switchToMainPile}>Take from Main</button></div></>
       <br></br>
       <button onClick={sendStartMessasge}>Start</button>
     </>
